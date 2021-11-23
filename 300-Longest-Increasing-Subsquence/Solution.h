8b37995e0d084aafd2c2816e48a5a9601e6419b4
wfsa <15001137580@163.com>
2021-11-23 10:09:32 +0800

    Update solution

--- 300-Longest-Increasing-Subsquence/Solution.h
+++ 300-Longest-Increasing-Subsquence/Solution.h
@@ -1,6 +1,18 @@
 class Solution {
 public:
+    // dynamic programming
     int lengthOfLIS(vector<int>& nums) {
-
+        const int N = nums.size();
+        int ans = 1;
+        vector<int> dp(N, 1);
+        for (int i = 1; i < N; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
     }
 };
